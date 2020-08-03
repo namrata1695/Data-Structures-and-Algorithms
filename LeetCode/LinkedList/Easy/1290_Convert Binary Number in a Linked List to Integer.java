3bec73616887010dd584f14d5b79242ab859f98c
namrata1695 <ajmerin@uwindsor.ca>
2020-08-03 19:25:10 -0400

    updating

--- LeetCode/LinkedList/Easy/1290_Convert Binary Number in a Linked List to Integer.java
+++ LeetCode/LinkedList/Easy/1290_Convert Binary Number in a Linked List to Integer.java
@@ -1,6 +1,7 @@
 Runtime: 0 ms, faster than 100.00% of Java online submissions for Convert Binary Number in a Linked List to Integer.
 Memory Usage: 36.9 MB, less than 100.00% of Java online submissions for Convert Binary Number in a Linked List to Integer.
 
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
